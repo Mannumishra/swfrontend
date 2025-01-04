0e7ed2a52d450ca716edba36c39e6460d9dd0faf
mannumishra <mannumishra2207@outlook.com>
2025-01-04 17:37:54 +0530

    add cgnages

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -57,20 +57,30 @@ const Events = () => {
             {data.map((item, index) => (
               <div className="col-md-3 col-sm-6 col-12" key={index}>
                 <div className="event-card shadow-sm rounded overflow-hidden">
-                  <Link to={`/event/${item._id}`}>
-                    <img
-                      src={item.eventImage}
-                      alt={item.eventName}
-                      className="img-fluid w-100"
-                    />
-                  </Link>
+                  {/* <Link to={`/event/${item._id}`}> */}
+                  <img
+                    src={item.eventImage}
+                    alt={item.eventName}
+                    className="img-fluid w-100"
+                  />
+                  {/* </Link> */}
                   <div className="event-card-body p-3">
-                    <Link
+                    {/* <Link
                       to={`/event/${item._id}`}
                       style={{ textDecoration: 'none', color: '#6E84D0' }} // Customize the color as needed
-                    >
-                      <h5 className="event-name mb-2 text-capitalize">{item.eventName}</h5>
-                    </Link>
+                    > */}
+                    <h5 className="event-name mb-2 text-capitalize">{item.eventName}</h5>
+                    <p className="event-date text-muted text-end">
+                      {new Intl.DateTimeFormat('en-GB', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                        // hour: '2-digit',
+                        // minute: '2-digit',
+                        // second: '2-digit',
+                      }).format(new Date(item.updatedAt))}
+                    </p>
+                    {/* </Link> */}
                   </div>
 
                 </div>
